4bb7cc0f155b7e70f1d9924748d6e14f74777fe3
Tomas Bernik <tomas.bernik@gmail.com>
2026-07-10 13:09:31 +0200

    pridanie prihlasovanie magic link a prechod na ostru verziu, uz nie demo

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR-SUPABASE-ANON-KEY";
+export const SUPABASE_URL = "https://zaibtcbpfjnraefxopsv.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_q13caChpMM7g11n5dFdTSA_n9XHlVCO";
